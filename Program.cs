ccb37aedf412f593bd74de03325d95c541bbbf68
Francisco Sousa <Francisco.Sousa@andritz.com>
2022-12-05 18:49:14 -0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_LINQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //LINQ e LAMBDA
+
+           // LAMBDA = (entrada) => (expressao)
+            
+            int[] lista = { 3, 9, 4, 6, 20, 10, 60, 90, 80, 50 }; 
+
+
+            var listaFiltrada = lista.Where(x => x >10 ).Select(a => a).OrderBy(a => a);
+
+
+            foreach (var item in listaFiltrada)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey(); 
+        }
+    }
+}
